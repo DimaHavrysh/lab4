115a8c03b1a3252b4a31e85149478dcee86156d0
DimaHavrysh <dima.gavrish2004@gmail.com>
2024-10-29 00:25:33 +0200

    prettier

--- src/storage.js
+++ src/storage.js
@@ -1,12 +1,12 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
 exports.Storage = void 0;
 class Storage {
-    static clear() {
-        localStorage.clear();
-    }
-    static remove(key) {
-        localStorage.removeItem(key);
-    }
+  static clear() {
+    localStorage.clear();
+  }
+  static remove(key) {
+    localStorage.removeItem(key);
+  }
 }
 exports.Storage = Storage;
